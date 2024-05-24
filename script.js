362f3a6c4fcff525e295316aba177942272e8239
F8179626 Paulo Rogerio Souza Da Costa <paulo.rsc@bb.com.br>
2024-05-23 22:43:57 -0300

    habittracker

---
+++ script.js
@@ -0,0 +1,7 @@
+function setGoalAndPenalty() {
+    var goalInput = document.getElementById("goal-input").value;
+    var penaltyInput = document.getElementById("penalty-input").value;
+    console.log("Meta Semanal:", goalInput);
+    console.log("Penalidade:", penaltyInput);
+    // Lógica para enviar a meta e a penalidade para o backend
+}
